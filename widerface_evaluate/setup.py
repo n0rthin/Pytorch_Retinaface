d3b1b2d0fbf60e98f2b2243814d72d78c8311741
Vlad Severin <severinv42@gmail.com>
2025-03-02 16:54:55 +0200

    make bbox build work on arm mac

--- widerface_evaluate/setup.py
+++ widerface_evaluate/setup.py
@@ -8,6 +8,23 @@ copyright@wondervictor
 from distutils.core import setup, Extension
 from Cython.Build import cythonize
 import numpy
+import platform
+import os
 
-package = Extension('bbox', ['box_overlaps.pyx'], include_dirs=[numpy.get_include()])
+extra_compile_args = []
+extra_link_args = []
+
+if platform.system() == 'Darwin' and platform.machine() == 'arm64':
+    os.environ["CFLAGS"] = "-arch arm64"
+    os.environ["ARCHFLAGS"] = "-arch arm64"
+    extra_compile_args.extend(["-arch", "arm64"])
+    extra_link_args.extend(["-arch", "arm64"])
+
+package = Extension(
+    'bbox',
+    ['box_overlaps.pyx'],
+    include_dirs=[numpy.get_include()],
+    extra_compile_args=extra_compile_args,
+    extra_link_args=extra_link_args
+)
 setup(ext_modules=cythonize([package]))
